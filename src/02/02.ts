9bd3f80e82b6f2b5a87e8da83d7ba0ba6b7c9a27
Evgeny Zuchkov <jenyazuchkov@gmail.com>
2026-07-30 19:24:21 +0300

    done 2 test

---
+++ src/02/02.ts
@@ -0,0 +1,55 @@
+type LocalCityType = {
+    title: string
+    countryTitle: string
+}
+
+type AddressType = {
+    streetTitle: string
+    city: LocalCityType
+}
+
+type TechType = {
+    id: number
+    title: string
+}
+
+type StudentType = {
+    name: string
+    age: number
+    isActive: boolean
+    address: AddressType
+    technologies: Array<TechType>
+}
+
+export const student: StudentType = {
+    name: "Jenya",
+    age: 31,
+    isActive: true,
+    address: {
+        streetTitle: "Surganova 2",
+        city: {
+            title: "Minsk",
+            countryTitle: "Belarus"
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: "HTML",
+        },
+        {
+            id: 2,
+            title: "CSS",
+        },
+        {
+            id: 3,
+            title: "React",
+        }
+
+    ]
+}
+
+console.log(student.age)
+console.log(student.name)
+console.log(student.address.city.title)
+console.log(student.technologies[0].title)
